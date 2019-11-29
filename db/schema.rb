b5c84396eca7f12e81156a0fba854e624fb620f0
tanizawa_kenta <kt10025102@gmail.com>
2019-11-29 14:28:48 +0900

    Ajax動作実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_092217) do
+ActiveRecord::Schema.define(version: 2019_11_28_093139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,13 @@ ActiveRecord::Schema.define(version: 2019_11_28_092217) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "blog_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
+  end
+
+  add_foreign_key "comments", "blogs"
 end
